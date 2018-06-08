4f2e587e27d101cfb8ba08c99b474cd66edf56e1
yangbinnnn <yangbinnnn@gmail.com>
2018-06-09 00:29:16 +0800

    update task

---
+++ 04_layout/src/task.js
@@ -0,0 +1,31 @@
+var tabs = document.querySelectorAll('.nav li a');
+var panels = document.querySelectorAll('.content section');
+
+for(i = 0; i < tabs.length; i++) {
+  var tab = tabs[i];
+  setTabHandler(tab, i);
+}
+
+function setTabHandler(tab, tabPos) {
+  tab.onclick = function() {
+    for(i = 0; i < tabs.length; i++) {
+      if(tabs[i].getAttribute('class')) {
+        tabs[i].removeAttribute('class');
+      }
+    }
+
+    tab.setAttribute('class', 'active');
+
+    panel = panels[tabPos]
+    if (panel === undefined) {
+        return
+    }
+
+    for(i = 0; i < panels.length; i++) {
+      if(panels[i].getAttribute('class')) {
+        panels[i].removeAttribute('class');
+      }
+    }
+    panel.setAttribute('class', 'active');
+  }
+}
